a75b4fdaa19b5f3763ab0c624f1005d42f913489
Piero Palevsky <ppalevsky@gmail.com>
2015-11-27 22:46:30 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sec_edgar_wrapper',
+      version='0.1.0',
+      author='Piero Palevsky',
+      author_email='ppalevsky@gmail.com',
+      install_requires=[
+        'beautifulsoup4==4.4.1',
+        'lxml==3.5.0',
+        'requests==2.8.1',
+        'xmltodict==0.9.2'
+      ],
+      description=('Python Wrapper for SEC Edgar, also contains some specific form parsers')
+      )
